17b3154ed810b120c7ed098a3f1eeac7e97591a4
Fathursyah Awandanu <fathur.syah04@gmail.com>
2025-07-22 10:21:39 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { Response, Request } from "express";
+import cors from 'cors';
+
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+
+const app = express();
+const PORT = 3000;
+
+// Express Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    credentials: true
+}));
+// Rate limiter middleware (100 requests per 15 minutes per IP)
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: 'Too many requests from this IP, please try again later.',
+});
+
+app.use(limiter); // apply to all requests
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
