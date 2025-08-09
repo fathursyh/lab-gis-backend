c4c8cc9fd310f6bac30807d79283e848f441017d
Fathursyah Awandanu <fathur.syah04@gmail.com>
2025-08-09 15:10:23 +0700

    update: add registration seeder

---
+++ src/database/seeders/registrationSeed.ts
@@ -0,0 +1,30 @@
+import { Event, Registration, User } from "../../models";
+
+
+async function seedRegistration() {
+    const registrations = [];
+    const users = await User.findAll({
+        attributes: ['id'],
+        limit: 10,
+    });
+    const events = await Event.findAll({
+        attributes: ['id'],
+        limit: 30,
+    });
+    for (let i = 0; i < 10; i++) {
+        const registration = {
+            userId: users[Math.round(Math.random() * 9)].dataValues.id,
+            eventId: events[Math.round(Math.random() * 29)].dataValues.id,
+        }
+        registrations.push(registration);
+    }
+    await Registration.bulkCreate(registrations);
+    console.log('Seeded 10 registrations');
+}
+
+seedRegistration()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
